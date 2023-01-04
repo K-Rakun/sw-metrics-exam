c8ceeccb85762e39a6373bd8942a12006d3461dc
Erman Şen <61985023+K-Rakun@users.noreply.github.com>
2023-01-04 13:25:34 +0300

    Add files via upload

--- math_test.py
+++ math_test.py
@@ -1,16 +1,21 @@
+"""Sw metrics exam"""
 import unittest
 import math
 
 class CalculatorTest(unittest.TestCase):
-
+    "Math test"
     def test_add(self):
+        "Test if addition is working"
         self.assertEqual(10, 3+7)
 
     def test_subtract(self):
+        "Test if subtraction is working"
         self.assertEqual(12,15 - 3)
 
     def test_factoriel(self):
+        "Test if factoriel is working"
         self.assertEqual(6, math.factorial(3) )
 
     def test_multiply(self):
+        "Test if multiplication is working"
         self.assertEqual(30, 5 * 6)
